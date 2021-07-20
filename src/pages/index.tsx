261400ec51354874f3ab193be088cda7d0682486
Rodrigo Eduardo <rodrigoow99@hotmail.com>
2021-07-20 16:08:24 -0300

    application structure done

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+
+      <h1>
+        Olá
+      </h1>
+    </>
+  )
+}
